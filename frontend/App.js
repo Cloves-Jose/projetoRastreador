aabcdcd33ecd34f604609c8a3646c5531bde9c2e
Cloves Jose <clovesjsilva8@gmail.com>
2022-03-10 06:47:20 -0300

    alteração seguranca

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,17 @@
 import { StatusBar, SafeAreaView } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Mapa from './src/screens/Map';
 
 export default function App() {
+
+  useEffect(() => {
+    async function sendServer(){
+      let response = await fetch('http://localhost:3000')
+    }
+  },[])
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar/>
